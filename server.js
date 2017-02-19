6742ea3d35911b52d67a34b3ea69f580a9941cbc
max <max.kusnadi@gmail.com>
2017-02-19 16:02:07 +0800

    babubyy

--- server.js
+++ server.js
@@ -1,35 +1,70 @@
+'use strict'
+
 // Express Server
 
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const port = 3000
+const pg = require('pg')
+const conString = 'postgres:///node-tutorial'
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.engine('.hbs', exphbs({
-	defaultLayout: 'main',
-	extname: '.hbs',
-	layoutsDir: path.join(__dirname, 'views/layouts')
-}))
+// pg.connect(conString, function(err, client, done) {
+// 	if (err) {
+// 		return console.error('error fetching client from pool', err)
+// 	}
+// 	client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function(err, result) {
+// 		done()
 
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+// 		if (err) {
+// 			return console.error('error happened during query', err)
+// 		}
+// 		console.log(result.rows[0])
+// 		process.exit(0)
+// 	})
+// })
 
-app.use((request, response, next) => {
-	console.log(request.headers)
-	next()
-})
+app.use(bodyParser.json())
+app.post('/users', (req, res, next) => {
+	const user = req.params
+	console.log(user)
 
-app.use((request, response, next) => {
-	request.chance = Math.random()
-	next()
-})
-
-app.get('/', (request, response) => {
-	response.render('home', {
-		name: 'John'
+	pg.connect(conString, (err, client, done) => {
+		if (err) {
+			return next(err)
+		}
+		client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], (err, result) => {
+			done()
+			if (err) {
+				return next(err)
+			}
+			res.send(200)
+		})
 	})
+})
+
+app.get('/users', (req, res, next) => {
+	pg.connect(conString, (err, client, done) => {
+		if (err) {
+			return next(err)
+		}
+		client.query('SELECT name, age FROM users;', [], (err, result) => {
+			done()
+			if (err) {
+				return next(err)
+			}
+			res.json(result.rows)
+
+		})
+	})
+})
+
+app.use((err, req, resp, next) => {
+	console.log(err)
+	resp.status(500).send('Something broke!')
 })
 
 app.listen(port, (err) => {
@@ -39,10 +74,45 @@ app.listen(port, (err) => {
 	console.log(`Server listens on ${port}`)
 })
 
+
+// Layout engine
+// app.engine('.hbs', exphbs({
+// 	defaultLayout: 'main',
+// 	extname: '.hbs',
+// 	layoutsDir: path.join(__dirname, 'views/layouts')
+// }))
+
+// app.set('view engine', '.hbs')
+// app.set('views', path.join(__dirname, 'views'))
+
+// app.use((request, response, next) => {
+// 	console.log(request.headers)
+// 	next()
+// })
+
+// app.use((request, response, next) => {
+// 	request.chance = Math.random()
+// 	next()
+// })
+
+// app.get('/', (request, response) => {
+// 	response.render('home', {
+// 		name: 'John'
+// 	})
+// })
+
+// app.listen(port, (err) => {
+// 	if (err) {
+// 		return console.log("Error has been found", err)
+// 	}
+// 	console.log(`Server listens on ${port}`)
+// })
+
 // app.use((err, req, resp, next) => {
 // 	console.log(err)
 // 	resp.status(500).send('Something broke!')
 // })
+
 // Normal Server
 // const http = require('http')
 // const port = 3000
