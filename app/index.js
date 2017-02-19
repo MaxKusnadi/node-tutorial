4e3c8d09e3470f3dc649bec27d02b11376cfae64
max <max.kusnadi@gmail.com>
2017-02-19 17:23:56 +0800

    finish authentication

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
-const calc = require('./calc')
+const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 
-const numbersToAdd = [
-	2,
-	1,
-	2130,
-	213
-]
-
-const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)
+const app = express()
+app.use(session({
+	store: new RedisStore({
+		url: config.redisStore.url
+	}),
+	secret: config.redisStore.secret,
+	resave: false,
+	saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
